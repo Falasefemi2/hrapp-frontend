851a789dd2b6a5bf41b727fd54307d0f3633b2a3
Falase Femi <femifalase228@gmail.com>
2025-10-18 21:34:22 +0100

    feat: backend url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,16 +2,16 @@
 
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://hrapp-6kwz.onrender.com";
 
 export const api = axios.create({
   baseURL: BASE_URL,
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
